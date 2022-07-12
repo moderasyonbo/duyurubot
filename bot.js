9a9033a3238f152d15dd01a426d70f14c0b3b4ac
novabotlist <93944142+moderasyonbo@users.noreply.github.com>
2022-07-12 18:52:06 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,77 @@
+
+ const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require("../ayarlar.json")
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+const Jimp = require('jimp');
+const db = require('quick.db');
+var prefix = ayarlar.prefix
+
+
+
+
+const log = message => {
+  console.log(`Giriş yaptım`);
+
+};
+
+
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let cmd = require(`./commands/${file}`);
+    let cmdFileName = file.split(".")[0];
+    console.log(`Komut Yükleme Çalışıyor: ${cmdFileName}`);
+    client.commands.set(cmd.help.name, cmd);
+  });
+});
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`Etkinlik Yükleme Çalışıyor: ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+
+
+
+
+
+
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === closly.sahip) permlvl = 4;
+  return permlvl;
+};
+
+
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+  
+
+client.login(closly.token);
